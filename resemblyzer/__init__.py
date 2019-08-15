90353332af3a87c4d65222a9c06192b49bafc59c
CorentinJ <corentin.jemine@gmail.com>
2019-08-15 19:02:14 +0200

    Initial commit

---
+++ resemblyzer/__init__.py
@@ -0,0 +1,4 @@
+from resemblyzer.encoder.inference import embed_utterance, embed_frames_batch, \
+    plot_embedding_as_heatmap, compute_partial_slices, embed_speaker, load_model, is_loaded
+from resemblyzer.resemblyzer import similarity
+
