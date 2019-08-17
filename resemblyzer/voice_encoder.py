54af7fbeb66cbbf26a4fd3a7a769def91ef517f7
CorentinJ <corentin.jemine@gmail.com>
2019-08-17 20:06:18 +0200

    Refactored the voice encoder and added more doc

--- resemblyzer/voice_encoder.py
+++ resemblyzer/voice_encoder.py
@@ -53,7 +53,7 @@ class VoiceEncoder(nn.Module):
         return embeds_raw / torch.norm(embeds_raw, dim=1, keepdim=True)
     
     @staticmethod
-    def compute_partial_slices(n_samples: int, min_coverage=0.75, overlap=0.5):
+    def compute_partial_slices(n_samples: int, rate, min_coverage):
         """
         Computes where to split an utterance waveform and its corresponding mel spectrogram to 
         obtain partial utterances of <partials_n_frames> each. Both the waveform and the 
@@ -61,26 +61,31 @@ class VoiceEncoder(nn.Module):
         correspond to its spectrogram.
     
         The returned ranges may be indexing further than the length of the waveform. It is 
-        recommended that you pad the waveform with zeros up to wave_slices[-1].stop.
+        recommended that you pad the waveform with zeros up to wav_slices[-1].stop.
     
         :param n_samples: the number of samples in the waveform
+        :param rate: how many partial utterances should occur per second. Partial utterances must 
+        cover the span of the entire utterance, thus the rate should not be lower than the inverse 
+        of the duration of a partial utterance. By default, partial utterances are 1.6s long and 
+        the minimum rate is thus 0.625.
         :param min_coverage: when reaching the last partial utterance, it may or may not have 
         enough frames. If at least <min_pad_coverage> of <partials_n_frames> are present, 
         then the last partial utterance will be considered by zero-padding the audio. Otherwise, 
         it will be discarded. If there aren't enough frames for one partial utterance, 
         this parameter is ignored so that the function always returns at least one slice.
-        :param overlap: by how much the partial utterance should overlap. If set to 0, the partial 
-        utterances are entirely disjoint. 
         :return: the waveform slices and mel spectrogram slices as lists of array slices. Index 
         respectively the waveform and the mel spectrogram with these slices to obtain the partial 
         utterances.
         """
-        assert 0 <= overlap < 1
         assert 0 < min_coverage <= 1
         
+        # Compute how many frames separate two partial utterances
         samples_per_frame = int((sampling_rate * mel_window_step / 1000))
         n_frames = int(np.ceil((n_samples + 1) / samples_per_frame))
-        frame_step = max(int(np.round(partials_n_frames * (1 - overlap))), 1)
+        frame_step = int(np.round((sampling_rate / rate) / samples_per_frame))
+        assert 0 < frame_step, "The rate is too high"
+        assert frame_step <= partials_n_frames, "The rate is too low, it should be %f at least" % \
+            (sampling_rate / (samples_per_frame * partials_n_frames))
         
         # Compute the slices
         wav_slices, mel_slices = [], []
@@ -100,27 +105,37 @@ class VoiceEncoder(nn.Module):
         
         return wav_slices, mel_slices
     
-    def embed_utterance(self, wav: np.ndarray, return_partials=False, **kwargs):
+    def embed_utterance(self, wav: np.ndarray, return_partials=False, rate=1.3, min_coverage=0.75):
         """
-        Computes an embedding for a single utterance.
+        Computes an embedding for a single utterance. The utterance is divided in partial 
+        utterances and an embedding is computed for each. The complete utterance embedding is the 
+        L2-normed average embedding of the partial utterances.
     
         :param wav: a preprocessed utterance waveform as a numpy array of float32
-        :param return_partials: if True, the partial embeddings will also be returned along with the 
-        wav slices that correspond to the partial embeddings.
-        :param kwargs: additional arguments to compute_partial_splits()
+        :param return_partials: if True, the partial embeddings will also be returned along with 
+        the wav slices corresponding to each partial utterance.
+        :param rate: how many partial utterances should occur per second. Partial utterances must 
+        cover the span of the entire utterance, thus the rate should not be lower than the inverse 
+        of the duration of a partial utterance. By default, partial utterances are 1.6s long and 
+        the minimum rate is thus 0.625.
+        :param min_coverage: when reaching the last partial utterance, it may or may not have 
+        enough frames. If at least <min_pad_coverage> of <partials_n_frames> are present, 
+        then the last partial utterance will be considered by zero-padding the audio. Otherwise, 
+        it will be discarded. If there aren't enough frames for one partial utterance, 
+        this parameter is ignored so that the function always returns at least one slice.
         :return: the embedding as a numpy array of float32 of shape (model_embedding_size,). If 
         <return_partials> is True, the partial utterances as a numpy array of float32 of shape 
         (n_partials, model_embedding_size) and the wav partials as a list of slices will also be 
-        returned. If <using_partials> is simultaneously set to False, both these values will be None 
-        instead.
+        returned.
         """
-        # Compute where to split the utterance into partials and pad if necessary
-        wave_slices, mel_slices = self.compute_partial_slices(len(wav), **kwargs)
-        max_wave_length = wave_slices[-1].stop
+        # Compute where to split the utterance into partials and pad the waveform with zeros if 
+        # the partial utterances cover a larger range. 
+        wav_slices, mel_slices = self.compute_partial_slices(len(wav), rate, min_coverage)
+        max_wave_length = wav_slices[-1].stop
         if max_wave_length >= len(wav):
             wav = np.pad(wav, (0, max_wave_length - len(wav)), "constant")
         
-        # Split the utterance into partials
+        # Split the utterance into partials and forward them through the model
         mel = audio.wav_to_mel_spectrogram(wav)
         mels = np.array([mel[s] for s in mel_slices])
         with torch.no_grad():
@@ -132,6 +147,6 @@ class VoiceEncoder(nn.Module):
         embed = raw_embed / np.linalg.norm(raw_embed, 2)
         
         if return_partials:
-            return embed, partial_embeds, wave_slices
+            return embed, partial_embeds, wav_slices
         return embed
     
