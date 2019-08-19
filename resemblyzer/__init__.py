2403a4af54753b07d44332c286e799aa4c1118aa
Corentin Jemine <corentin.jemine@gmail.com>
2019-08-19 12:36:47 +0200

    Package is now pypi-compliant (#1)
    * Added setup.py
    
    * Included the pretrained model in the package

--- resemblyzer/__init__.py
+++ resemblyzer/__init__.py
@@ -1,3 +1,5 @@
+name = "resemblyzer"
+
 from resemblyzer.audio import preprocess_wav, wav_to_mel_spectrogram, trim_long_silences, \
     normalize_volume
 from resemblyzer.voice_encoder import VoiceEncoder
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.MD", "r") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="Resemblyzer",
+    version="0.0.1-dev",
+    packages=find_packages(),
+    package_data={
+        "resemblyzer": ["pretrained.pt"]
+    },
+    install_requires=requirements,
+    author="Corentin Jemine",
+    author_email="corentin@resemble.ai",
+    description="Analyze and compare voices with deep learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/resemble-ai/Resemblyzer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
