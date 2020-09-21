82211ae6de85da21373d803e4c92d69ae69f815e
yesseyev <yesseyev.mt@gmail.com>
2020-09-21 17:38:26 +0600

    "weights_fpath" load fixes

--- resemblyzer/voice_encoder.py
+++ resemblyzer/voice_encoder.py
@@ -11,10 +11,10 @@ import torch
 class VoiceEncoder(nn.Module):
     def __init__(self, device: Union[str, torch.device]=None, verbose=True, weights_fpath: Union[Path, str]=None):
         """
-        :param device: either a torch device or the name of a torch device (e.g. "cpu", "cuda").
-        :param weights_fpath: path to "<CUSTOM_MODEL>.pt" file path
-        If None, defaults to cuda if it is available on your machine, otherwise the model will 
+        If None, defaults to cuda if it is available on your machine, otherwise the model will
         run on cpu. Outputs are always returned on the cpu, as numpy arrays.
+        :param weights_fpath: path to "<CUSTOM_MODEL>.pt" file path.
+        If None, defaults to built-in "pretrained.pt" model
         """
         super().__init__()
 
@@ -31,9 +31,9 @@ class VoiceEncoder(nn.Module):
         self.device = device
 
         # Load the pretrained model'speaker weights
-        if not weights_fpath:
+        if weights_fpath is None:
             weights_fpath = Path(__file__).resolve().parent.joinpath("pretrained.pt")
-        elif isinstance(weights_fpath, str):
+        else:
             weights_fpath = Path(weights_fpath)
 
         if not weights_fpath.exists():
@@ -52,12 +52,12 @@ class VoiceEncoder(nn.Module):
         """
         Computes the embeddings of a batch of utterance spectrograms.
 
-        :param mels: a batch of mel spectrograms of same duration as a float32 tensor of shape 
-        (batch_size, n_frames, n_channels) 
-        :return: the embeddings as a float 32 tensor of shape (batch_size, embedding_size). 
+        :param mels: a batch of mel spectrograms of same duration as a float32 tensor of shape
+        (batch_size, n_frames, n_channels)
+        :return: the embeddings as a float 32 tensor of shape (batch_size, embedding_size).
         Embeddings are positive and L2-normed, thus they lay in the range [0, 1].
         """
-        # Pass the input through the LSTM layers and retrieve the final hidden state of the last 
+        # Pass the input through the LSTM layers and retrieve the final hidden state of the last
         # layer. Apply a cutoff to 0 for negative values and L2 normalize the embeddings.
         _, (hidden, _) = self.lstm(mels)
         embeds_raw = self.relu(self.linear(hidden[-1]))
@@ -66,26 +66,26 @@ class VoiceEncoder(nn.Module):
     @staticmethod
     def compute_partial_slices(n_samples: int, rate, min_coverage):
         """
-        Computes where to split an utterance waveform and its corresponding mel spectrogram to 
-        obtain partial utterances of <partials_n_frames> each. Both the waveform and the 
-        mel spectrogram slices are returned, so as to make each partial utterance waveform 
+        Computes where to split an utterance waveform and its corresponding mel spectrogram to
+        obtain partial utterances of <partials_n_frames> each. Both the waveform and the
+        mel spectrogram slices are returned, so as to make each partial utterance waveform
         correspond to its spectrogram.
-    
-        The returned ranges may be indexing further than the length of the waveform. It is 
+
+        The returned ranges may be indexing further than the length of the waveform. It is
         recommended that you pad the waveform with zeros up to wav_slices[-1].stop.
-    
+
         :param n_samples: the number of samples in the waveform
-        :param rate: how many partial utterances should occur per second. Partial utterances must 
-        cover the span of the entire utterance, thus the rate should not be lower than the inverse 
-        of the duration of a partial utterance. By default, partial utterances are 1.6s long and 
+        :param rate: how many partial utterances should occur per second. Partial utterances must
+        cover the span of the entire utterance, thus the rate should not be lower than the inverse
+        of the duration of a partial utterance. By default, partial utterances are 1.6s long and
         the minimum rate is thus 0.625.
-        :param min_coverage: when reaching the last partial utterance, it may or may not have 
-        enough frames. If at least <min_pad_coverage> of <partials_n_frames> are present, 
-        then the last partial utterance will be considered by zero-padding the audio. Otherwise, 
-        it will be discarded. If there aren't enough frames for one partial utterance, 
+        :param min_coverage: when reaching the last partial utterance, it may or may not have
+        enough frames. If at least <min_pad_coverage> of <partials_n_frames> are present,
+        then the last partial utterance will be considered by zero-padding the audio. Otherwise,
+        it will be discarded. If there aren't enough frames for one partial utterance,
         this parameter is ignored so that the function always returns at least one slice.
-        :return: the waveform slices and mel spectrogram slices as lists of array slices. Index 
-        respectively the waveform and the mel spectrogram with these slices to obtain the partial 
+        :return: the waveform slices and mel spectrogram slices as lists of array slices. Index
+        respectively the waveform and the mel spectrogram with these slices to obtain the partial
         utterances.
         """
         assert 0 < min_coverage <= 1
@@ -118,31 +118,31 @@ class VoiceEncoder(nn.Module):
 
     def embed_utterance(self, wav: np.ndarray, return_partials=False, rate=1.3, min_coverage=0.75):
         """
-        Computes an embedding for a single utterance. The utterance is divided in partial 
-        utterances and an embedding is computed for each. The complete utterance embedding is the 
+        Computes an embedding for a single utterance. The utterance is divided in partial
+        utterances and an embedding is computed for each. The complete utterance embedding is the
         L2-normed average embedding of the partial utterances.
-        
+
         TODO: independent batched version of this function
-    
+
         :param wav: a preprocessed utterance waveform as a numpy array of float32
-        :param return_partials: if True, the partial embeddings will also be returned along with 
+        :param return_partials: if True, the partial embeddings will also be returned along with
         the wav slices corresponding to each partial utterance.
-        :param rate: how many partial utterances should occur per second. Partial utterances must 
-        cover the span of the entire utterance, thus the rate should not be lower than the inverse 
-        of the duration of a partial utterance. By default, partial utterances are 1.6s long and 
+        :param rate: how many partial utterances should occur per second. Partial utterances must
+        cover the span of the entire utterance, thus the rate should not be lower than the inverse
+        of the duration of a partial utterance. By default, partial utterances are 1.6s long and
         the minimum rate is thus 0.625.
-        :param min_coverage: when reaching the last partial utterance, it may or may not have 
-        enough frames. If at least <min_pad_coverage> of <partials_n_frames> are present, 
-        then the last partial utterance will be considered by zero-padding the audio. Otherwise, 
-        it will be discarded. If there aren't enough frames for one partial utterance, 
+        :param min_coverage: when reaching the last partial utterance, it may or may not have
+        enough frames. If at least <min_pad_coverage> of <partials_n_frames> are present,
+        then the last partial utterance will be considered by zero-padding the audio. Otherwise,
+        it will be discarded. If there aren't enough frames for one partial utterance,
         this parameter is ignored so that the function always returns at least one slice.
-        :return: the embedding as a numpy array of float32 of shape (model_embedding_size,). If 
-        <return_partials> is True, the partial utterances as a numpy array of float32 of shape 
-        (n_partials, model_embedding_size) and the wav partials as a list of slices will also be 
+        :return: the embedding as a numpy array of float32 of shape (model_embedding_size,). If
+        <return_partials> is True, the partial utterances as a numpy array of float32 of shape
+        (n_partials, model_embedding_size) and the wav partials as a list of slices will also be
         returned.
         """
-        # Compute where to split the utterance into partials and pad the waveform with zeros if 
-        # the partial utterances cover a larger range. 
+        # Compute where to split the utterance into partials and pad the waveform with zeros if
+        # the partial utterances cover a larger range.
         wav_slices, mel_slices = self.compute_partial_slices(len(wav), rate, min_coverage)
         max_wave_length = wav_slices[-1].stop
         if max_wave_length >= len(wav):
@@ -165,9 +165,9 @@ class VoiceEncoder(nn.Module):
 
     def embed_speaker(self, wavs: List[np.ndarray], **kwargs):
         """
-        Compute the embedding of a collection of wavs (presumably from the same speaker) by 
+        Compute the embedding of a collection of wavs (presumably from the same speaker) by
         averaging their embedding and L2-normalizing it.
-        
+
         :param wavs: list of wavs a numpy arrays of float32.
         :param kwargs: extra arguments to embed_utterance()
         :return: the embedding as a numpy array of float32 of shape (model_embedding_size,).
